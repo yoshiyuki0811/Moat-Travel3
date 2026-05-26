6d50cd3932f46a78108e43e6c727278414f0f6bf
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-26 14:27:50 +0900

    ファーストコミット

---
+++ moattravel3/src/main/java/com/example/moattravel2/Moattravel3Application.java
@@ -0,0 +1,13 @@
+package com.example.moattravel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moattravel3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moattravel3Application.class, args);
+	}
+
+}
---
+++ moattravel3/src/test/java/com/example/moattravel2/Moattravel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moattravel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moattravel3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
