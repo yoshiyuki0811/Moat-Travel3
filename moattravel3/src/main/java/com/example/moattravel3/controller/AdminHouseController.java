a1a32d3af80ea4dd5fa330dda257f5bcfb932832
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-26 19:10:51 +0900

    １５章民宿の更新機能の実装

--- moattravel3/src/main/java/com/example/moattravel3/controller/AdminHouseController.java
+++ moattravel3/src/main/java/com/example/moattravel3/controller/AdminHouseController.java
@@ -108,5 +108,20 @@ public class AdminHouseController {
 		
 		return "admin/houses/edit";
 	}
+	
+	@PostMapping("{id}/update")
+	public String update(@ModelAttribute @Validated HouseEditForm houseEditForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			
+			return "admin/houses/edit";
+		}
+		
+		houseService.update(houseEditForm);
+		
+		redirectAttributes.addFlashAttribute("succesMessage", "民宿情報を編集しました。");
+		
+		return "redirect:/admin/houses";
+	}
 
 }
--- moattravel3/src/main/java/com/example/moattravel3/servive/HouseService.java
+++ moattravel3/src/main/java/com/example/moattravel3/servive/HouseService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.moattravel3.entity.House;
+import com.example.moattravel3.form.HouseEditForm;
 import com.example.moattravel3.form.HouseRegisterForm;
 import com.example.moattravel3.repository.HouseRepository;
 
@@ -22,62 +23,99 @@ import lombok.RequiredArgsConstructor;
 public class HouseService {
 
 	private final HouseRepository houseRepository;
-	
+
 	@Transactional
 	public void create(HouseRegisterForm houseRegisterForm) {
-		
-		House house =new House();
-		
+
+		House house = new House();
+
 		MultipartFile imageFile = houseRegisterForm.getImageFile();
-		
-		if(!imageFile.isEmpty()) {
-			
+
+		if (!imageFile.isEmpty()) {
+
 			String imageName = imageFile.getOriginalFilename();
-			
+
 			String hashedImageName = generateNewFileName(imageName);
-			
+
 			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
-			
+
 			copyImageFile(imageFile, filePath);
-			
+
 			house.setImageName(hashedImageName);
 		}
-		house.setName(houseRegisterForm.getName());                
+		house.setName(houseRegisterForm.getName());
 		house.setDescription(houseRegisterForm.getDescription());
 		house.setPrice(houseRegisterForm.getPrice());
 		house.setCapacity(houseRegisterForm.getCapacity());
 		house.setPostalCode(houseRegisterForm.getPostalCode());
 		house.setAddress(houseRegisterForm.getAddress());
 		house.setPhoneNumber(houseRegisterForm.getPhoneNumber());
-		                     
+
 		houseRepository.save(house);
 
 	}
-	
+
+	@Transactional
+	public void update(HouseEditForm houseEditForm) {
+		House house = houseRepository.getReferenceById(houseEditForm.getId());
+
+		MultipartFile imageFile = houseEditForm.getImageFile();
+
+		if (!imageFile.isEmpty()) {
+
+			String imageName = imageFile.getOriginalFilename();
+
+			String hashedImageName = generateNewFileName(imageName);
+
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+
+			copyImageFile(imageFile, filePath);
+
+			house.setImageName(hashedImageName);
+		}
+		house.setName(houseEditForm.getName());
+
+		house.setDescription(houseEditForm.getDescription());
+
+		house.setPrice(houseEditForm.getPrice());
+
+		house.setCapacity(houseEditForm.getCapacity());
+
+		house.setPostalCode(houseEditForm.getPostalCode());
+
+		house.setAddress(houseEditForm.getAddress());
+
+		house.setPhoneNumber(houseEditForm.getPhoneNumber());
+
+		houseRepository.save(house);
+
+	}
+
 	//UUIDを使って生成したファイル名を返す
 	public String generateNewFileName(String fileName) {
-		
+
 		String[] fileNames = fileName.split("\\.");
-		
-		for(int i = 0; i<fileNames.length -1; i++) {
-			
-			fileNames[i] =UUID.randomUUID().toString();
+
+		for (int i = 0; i < fileNames.length - 1; i++) {
+
+			fileNames[i] = UUID.randomUUID().toString();
 		}
-		
+
 		String hashedFileName = String.join(".", fileNames);
-		
+
 		return hashedFileName;
 	}
+
 	//画像ファイルを指定したfileにコピーする
 	public void copyImageFile(MultipartFile imageFile, Path filePath) {
-		
+
 		try {
 			Files.copy(imageFile.getInputStream(), filePath);
-		
-		}catch(IOException e) {
-			
+
+		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 	}
-	
+
 }
